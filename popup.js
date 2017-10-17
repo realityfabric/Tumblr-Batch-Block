7beb8fb5b73836a3da59ba851a69c8e06be14fef
realityfabric <realityfabric@gmail.com>
2017-10-17 15:28:27 -0700

    Fix indentation in popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('blockButton').addEventListener('click',
+        function () {
+	    chrome.tabs.executeScript(null, {file: "jquery.min.js"},
 		function () {
-			chrome.tabs.executeScript(null, {file: "jquery.min.js"},
-				function () {
-                    chrome.tabs.executeScript(null, {file: "contentscript.js"});
+		    chrome.tabs.executeScript(null, {file: "contentscript.js"});
                 }
             );
         }
     );
 
     document.getElementById('exportBlockListButton').addEventListener('click',
+	function () {
+	    chrome.tabs.executeScript(null, {file: 'jquery.min.js'},
 		function () {
-			chrome.tabs.executeScript(null, {file: 'jquery.min.js'},
-				function () {
-					chrome.tabs.executeScript(null, {file: 'export.js'});
-				}
-			);
+		    chrome.tabs.executeScript(null, {file: 'export.js'});
 		}
-	);
+	    );
+	}
+    );
 });
