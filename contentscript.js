1d024506cfca2cc7e845daa742f12d8c19fa1cff
realityfabric <realityfabric@gmail.com>
2018-12-04 21:43:32 -0800

    Clean list of users getting blocked
    Prevents duplicates both between the list to be blocked and those already blocked and within the list itself.
    This is necessary for a planned fix. That fix will check the last blocked user to verify that it was successful. Removing duplicates will add some up-front time (more for users who have many blocks), but will greatly reduce the time taken to ensure proper blocks (planning a timeout feature).

--- contentscript.js
+++ contentscript.js
@@ -17,19 +17,87 @@ var str = str.replace(" ", "");
 
 var arr = str.split(",");
 
+// expandBlockList clicks the button which opens the blocklist
+// Then it clicks the 'Show More' button until it is no longer visible
+// The 'Show More' button still exists, and will loop through the block list if you continue to click it.
+// TODO: this was copied as a part of a hotfix from export.js
+var expandBlockList = function (callback) {
+    $('document').ready( function () {
+        $("#blocked_blogs > .accordion_trigger_wrapper > .accordion_trigger").click();
+
+        var expandInterval = setInterval(function() {
+            if ($('#blocked_blogs > div.accordion_content.show > button').is(':visible')) {
+                $('#blocked_blogs > div.accordion_content.show > button').click();
+            } else {
+                clearInterval(expandInterval);
+                callback();
+            }
+        }, 500);
+    });
+}
+
+// getBlockList gathers all the blog names in a user's block list
+// It returns the list as an array
+// TODO: this was copied as a part of a hotfix from export.js
+var getBlockList = function () {
+    var i = 1;
+    var blocklist = [];
+
+    var blog = jQuery('#blocked_blogs > div.accordion_content.show > ul > li:nth-child(' + i++ + ') > div.blog-info > a.name');
+
+    while (blog !== undefined && blog[0] !== undefined) {
+        blocklist.push(blog[0].innerText);
+
+        var blog = jQuery('#blocked_blogs > div.accordion_content.show > ul > li:nth-child(' + i++ + ') > div.blog-info > a.name');
+    }
+
+    return blocklist;
+}
+
+// removes blogs that are already blocked from list to be blocked
+// newBlocks: array of blogs to be blocked
+// TODO: return list of duplicates
+var cleanBlocklist = function (newBlocks) {
+    var existingBlocks = getBlockList();
+    var uniqueBlocks = [];
+
+    // https://stackoverflow.com/a/9229932
+    $.each(newBlocks, function(i, el) {
+        // if the element doesn't already exist in uniqueBlocks, add it
+        if($.inArray(el, existingBlocks) === -1) {
+            uniqueBlocks.push(el);
+        }
+    });
+
+    var reallyUniqueBlocks = [];
+
+    $.each(uniqueBlocks, function (i, el) {
+        if($.inArray(el, reallyUniqueBlocks) === -1) {
+            reallyUniqueBlocks.push(el);
+        }
+    });
+
+    return reallyUniqueBlocks;
+}
+
 var activate = function () {
     console.log ("activate");
-    openBlockList(arr[0]);
 
-    var i = 1;
-    var inter = setInterval (function () {
-        console.log (i + " -- " + arr[i]);
-        block(arr[i++]);
-        if (i >= arr.length) {
-            clearInterval(inter);
-            console.log ("exit");
-        }
-    }, 1100);
+    expandBlockList(function() {
+        arr = cleanBlocklist(arr);
+
+        openBlockList(arr[0]);
+
+        var i = 1;
+        var inter = setInterval (function () {
+            console.log (i + " -- " + arr[i]);
+            block(arr[i++]);
+            if (i >= arr.length) {
+                clearInterval(inter);
+                console.log ("exit");
+            }
+        }, 1100);
+    });
 }
 
 activate ();
