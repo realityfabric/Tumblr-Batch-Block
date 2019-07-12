f609a8045572c465da1e228be5e83d16585994de
realityfabric <realityfabric@gmail.com>
2019-07-11 21:51:09 -0700

    Add MutationObserver console logs
    This is the preface to better iteration over the block list. Hopefully this will work better than setInterval (faster for fast computers, actually functional for slower ones).

--- contentscript.js
+++ contentscript.js
@@ -18,6 +18,38 @@ var activate = function () {
 
 	openBlockList();
 
+	// we will be watching the blockListContainer for changes
+	var blockListContainer = $('#blocked_blogs > div.accordion_content');
+
+	// i don't know if i need all this to do what i need to do
+	// TODO: determine what to keep and what to trim
+	var mutationObserverConfig = { attributes: true, childList: true, subtree: true };
+
+	var mutationObserverCallback = function (mutationsList, observer) {
+		for (var mutation of mutationsList) {
+			if (mutation.type == 'childList') {
+				if (mutation.target.classList[0] == "error_tag") {
+					console.log("Error");
+				}
+				else if (mutation.addedNodes.length > 0) {
+					if ((mutation.addedNodes[0]).getAttribute('data-tumblelog') !== null) {
+						console.log("Added to blocklist: " + (mutation.addedNodes[0]).getAttribute('data-tumblelog'));
+					}
+				}
+				else if (mutation.removedNodes.length > 0) {
+					if ((mutation.removedNodes[0]).getAttribute('data-tumblelog') !== null) {
+						console.log("Removed from blocklist: " + (mutation.removedNodes[0]).getAttribute('data-tumblelog'));
+					}
+				}
+			}
+		}
+	};
+
+	var observer = new MutationObserver(mutationObserverCallback);
+
+	// in the console you don't need [0], but in the extension you do... a frustrating adventure in troubleshooting
+	observer.observe(blockListContainer[0], mutationObserverConfig);
+
     var i = 0;
     var inter = setInterval (function () {
         console.log (i + " -- " + usersToBeBlocked[i]);
@@ -27,6 +59,8 @@ var activate = function () {
             console.log ("exit");
         }
     }, 1100);
+
+	// observer.disconnect() // this is disabled because it triggers before any of the blocking happens
 }
 
 activate ();
