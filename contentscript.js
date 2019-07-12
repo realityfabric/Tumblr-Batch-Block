e55103bcbb711cf0041a91d743ffc473672bc088
realityfabric <realityfabric@gmail.com>
2019-07-12 12:15:52 -0700

    Convert to looping with MutationObserver
    Reusing code from export.js to extract an array of currently blocked users.
    Filtering the user input so that only users that aren't already blocked will be handled.
    Remove the setTimeouts and setIntervals (no longer needed, because next block is triggered by the completion of the previous block).
    
    No longer using openBlockList, because it is already handled by expandBlockList.
    
    This is MUCH FASTER, and I tested it against more than 500 users. I probably need to test it on a very slow computer, but this should make computer speed irrelevant for accuracy.

--- contentscript.js
+++ contentscript.js
@@ -1,43 +1,54 @@
 var openBlockList = function () {
-        $('document').ready( function () {
-            $("#blocked_blogs > .accordion_trigger_wrapper > .accordion_trigger").click();
-        });
+	$('document').ready( function () {
+		$("#blocked_blogs > .accordion_trigger_wrapper > .accordion_trigger").click();
+	});
 }
 
 var block = function (blog) {
     $("#blocked_blogs > .accordion_content > .block-input > .text > #block").val(blog);
-    setTimeout(function () {
-        $("#blocked_blogs > .accordion_content > .block-input > .block-button").click();
-    }, 500);
+    $("#blocked_blogs > .accordion_content > .block-input > .block-button").click();
 }
 
-var activate = function () {
+/* activate
+ * blocks an array of users
+ * @usersToBeBlocked an array of usernames (should be prestripped of already blocked users)
+ */
+var activate = function (usersToBeBlocked) {
     console.log ("activate");
-	var usersToBeBlockedStr = window.prompt("Please input comma separated list of users to be blocked","");
-	var usersToBeBlocked = usersToBeBlockedStr.split(",");
-
-	openBlockList();
 
 	// we will be watching the blockListContainer for changes
-	var blockListContainer = $('#blocked_blogs > div.accordion_content');
+	var blockListContainer = $('#blocked_blogs > div.accordion_content.show');
 
 	// i don't know if i need all this to do what i need to do
 	// TODO: determine what to keep and what to trim
 	var mutationObserverConfig = { attributes: true, childList: true, subtree: true };
 
+	var i = 0;
 	var mutationObserverCallback = function (mutationsList, observer) {
 		for (var mutation of mutationsList) {
 			if (mutation.type == 'childList') {
 				if (mutation.target.classList[0] == "error_tag") {
-					console.log("Error");
+					console.log("Error on " + usersToBeBlocked[i]);
+					block(usersToBeBlocked[++i]);
 				}
 				else if (mutation.addedNodes.length > 0) {
 					var tumblelog = (mutation.addedNodes[0]).getAttribute('data-tumblelog');
 					if (tumblelog !== null) {
-						console.log("Added to blocklist: " + tumblelog);
+						if (i == 0 && tumblelog == usersToBeBlocked[0]) {
+							console.log("Added to blocklist: " + tumblelog);
+							block(usersToBeBlocked[++i]);
+						} else if (i !== 0) {
+							console.log("Added to blocklist: " + tumblelog);
+							block(usersToBeBlocked[++i]);
+						}
 					}
 				}
 			}
+		}
+
+		// this seems like it listens for one element extra, but i'm not sure
+		if (i > usersToBeBlocked.length) {
+			observer.disconnect();
 		}
 	};
 
@@ -45,18 +56,59 @@ var activate = function () {
 
 	// in the console you don't need [0], but in the extension you do... a frustrating adventure in troubleshooting
 	observer.observe(blockListContainer[0], mutationObserverConfig);
-
-    var i = 0;
-    var inter = setInterval (function () {
-        console.log (i + " -- " + usersToBeBlocked[i]);
-        block(usersToBeBlocked[i++]);
-        if (i >= usersToBeBlocked.length) {
-            clearInterval(inter);
-            console.log ("exit");
-        }
-    }, 1100);
-
-	// observer.disconnect() // this is disabled because it triggers before any of the blocking happens
+	// trigger a cascade of MutationObserver events (starts iteration through the blocklist)
+	block(usersToBeBlocked[0]);
 }
 
-activate ();
+// expandBlockList clicks the button which opens the blocklist
+// Then it clicks the 'Show More' button until it is no longer visible
+// The 'Show More' button still exists, and will loop through the block list if you continue to click it.
+var expandBlockList = function (callback) {
+    $('document').ready(function () {
+		$("#blocked_blogs > .accordion_trigger_wrapper > .accordion_trigger").click();
+
+		var expandInterval = setInterval(function() {
+			if ($('#blocked_blogs > div.accordion_content.show > button').is(':visible')) {
+				$('#blocked_blogs > div.accordion_content.show > button').click();
+			} else {
+				clearInterval(expandInterval);
+				callback();
+			}
+		}, 500);
+    });
+}
+
+// getBlockList gathers all the blog names in a user's block list
+// It returns the list as an array
+var getBlockList = function () {
+    var i = 1;
+    var blocklist = [];
+
+    var blog = jQuery('#blocked_blogs > div.accordion_content.show > ul > li:nth-child(' + i++ + ') > div.blog-info > a.name');
+
+    while (blog !== undefined && blog[0] !== undefined) {
+        blocklist.push(blog[0].innerText);
+
+        var blog = jQuery('#blocked_blogs > div.accordion_content.show > ul > li:nth-child(' + i++ + ') > div.blog-info > a.name');
+    }
+
+    return blocklist;
+}
+
+// get list of users who will be blocked
+var usersToBeBlockedStr = window.prompt("Please input comma separated list of users to be blocked","");
+// convert user input to array
+var usersToBeBlocked = usersToBeBlockedStr.split(",");
+// trim whitespace (necessary for filtering)
+for (var index = 0; index < usersToBeBlocked.length; index++) {
+	usersToBeBlocked[index] = usersToBeBlocked[index].trim();
+}
+expandBlockList(function() {
+	// get list of users who are already blocked
+    var blockedUsers = JSON.stringify(getBlockList());
+	// remove already blocked users from user input
+	usersToBeBlocked = usersToBeBlocked.filter(user => !(blockedUsers.includes(user)));
+
+	activate(usersToBeBlocked);
+});
+
